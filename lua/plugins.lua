3d00a6a1517bf2dd52f3a6897884636b0dc8bf64
Ming Chang <mail@mingchang.tw>
2026-06-16 16:56:15 +0800

    260616: MacBook Pro 2.0.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,44 +1,36 @@
-vim.cmd [[packadd packer.nvim]]
-return require('packer').startup(function(use)
-    use('wbthomason/packer.nvim')
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('mbbill/undotree')
-    use('neovim/nvim-lspconfig')
-    use('hrsh7th/nvim-cmp')
-    use('hrsh7th/cmp-buffer')
-    use('hrsh7th/cmp-path')
-    use('saadparwaiz1/cmp_luasnip')
-    use('hrsh7th/cmp-nvim-lsp')
-    use('hrsh7th/cmp-nvim-lua')
-    use('L3MON4D3/LuaSnip')
-    use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons"
-    }
-    use("williamboman/mason.nvim")
-    use("xiyaowong/transparent.nvim")
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons',
-        },
-    }
-    use {
+return require('pckr').add{
+    'wbthomason/packer.nvim';
+    {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'};
+    'mbbill/undotree';
+    'neovim/nvim-lspconfig';
+    'hrsh7th/nvim-cmp';
+    'hrsh7th/cmp-buffer';
+    'hrsh7th/cmp-path';
+    'saadparwaiz1/cmp_luasnip';
+    'hrsh7th/cmp-nvim-lsp';
+    'hrsh7th/cmp-nvim-lua';
+    'L3MON4D3/LuaSnip';
+    'folke/trouble.nvim';
+    'kyazdani42/nvim-web-devicons';
+    'williamboman/mason.nvim';
+    'xiyaowong/transparent.nvim';
+    'nvim-tree/nvim-tree.lua';
+    'nvim-tree/nvim-web-devicons';
+    {
         'saecki/crates.nvim',
         tag = 'v0.3.0',
         requires = { 'nvim-lua/plenary.nvim' },
         config = function()
             require('crates').setup()
-        end,
-    }
-    use('jose-elias-alvarez/null-ls.nvim')
-    use {
+        end
+    };
+    {
         'dinhhuy258/git.nvim',
         config = function()
             require('git').setup()
         end
-    }
-    use("sts10/vim-pink-moon")
-    use('mrcjkb/rustaceanvim')
-    use('MunifTanjim/nougat.nvim')
-end)
+    };
+    'sts10/vim-pink-moon';
+    'mrcjkb/rustaceanvim';
+    'MunifTanjim/nougat.nvim';
+}
